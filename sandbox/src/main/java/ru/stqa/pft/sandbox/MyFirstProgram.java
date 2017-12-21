ab3c88d666e44121ec9cc87d4d5b4762b1f482d4
vitaliy4us <vitaliy4us@yahoo.com>
2017-12-21 10:31:12 +0200

    Созданы классы квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,14 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class MyFirstProgram {
 	public static void main(String[] arg) {
 		System.out.println("Hello World");
+
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной равной " + s.a + " = " + s.area() );
+		Rectangle r = new Rectangle(6, 8);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area() );
 	}
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+    public double a;
+
+    public Square(double a) {
+        this.a = a;
+    }
+
+    public double area() {
+        return a * a;
+    }
+}
