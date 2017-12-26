8f79d2b981708d0c691883c66caf4b9b2415c6f2
vitaliy4us <vitaliy4us@yahoo.com>
2017-12-26 14:22:57 +0200

    Revert "Revert "Наследование заменено делегирование""
    This reverts commit f2ccb5d656ac31e198cbce6c330422297d9a2b5a.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicatonManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicatonManager.java
@@ -1,15 +1,18 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
-    FirefoxDriver wd;
+public class ApplicatonManager {
+    //FirefoxDriver wd;
+    //ChromeDriver wd;
+    //InternetExplorerDriver wd;
+    EdgeDriver wd;
 
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
@@ -20,16 +23,17 @@ public class TestBase {
         }
     }
 
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
+    public void init() {
+        //wd = new FirefoxDriver();
+        //wd = new ChromeDriver();
+        //wd = new InternetExplorerDriver();
+        wd = new EdgeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         login("admin", "secret");
-
     }
 
-    private void login(String username, String password) {
+    public void login(String username, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
@@ -39,15 +43,15 @@ public class TestBase {
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
-    protected void returnToGroupPage() {
+    public void returnToGroupPage() {
         wd.findElement(By.linkText("group page")).click();
     }
 
-    protected void submitGroupCreation() {
+    public void submitGroupCreation() {
         wd.findElement(By.name("submit")).click();
     }
 
-    protected void fillGroupForm(GroupData groupData) {
+    public void fillGroupForm(GroupData groupData) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
         wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
@@ -59,24 +63,23 @@ public class TestBase {
         wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
 
-    protected void initGroupCreation() {
+    public void initGroupCreation() {
         wd.findElement(By.name("new")).click();
     }
 
-    protected void gotoGroupPage() {
+    public void gotoGroupPage() {
         wd.findElement(By.linkText("groups")).click();
     }
 
-    @AfterMethod
-    public void tearDown() {
+    public void stop() {
         wd.quit();
     }
 
-    protected void deleteSelectedGroup() {
+    public void deleteSelectedGroup() {
         wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
     }
 
-    protected void selectGroup() {
+    public void selectGroup() {
         if (!wd.findElement(By.name("selected[]")).isSelected()) {
             wd.findElement(By.name("selected[]")).click();
         }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.ApplicatonManager;
+
+public class TestBase {
+
+    protected final ApplicatonManager app = new ApplicatonManager();
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        app.stop();
+    }
+
+}
